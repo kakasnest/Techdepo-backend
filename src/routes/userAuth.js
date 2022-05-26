1cfa8dd2d1b4ceb2f56084f09dea0821a1c2ac9b
Adam <kakasnest@gmail.com>
2022-05-26 23:08:35 +0200

    Filed renamed

--- src/routes/userAuth.js
+++ src/routes/userAuth.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { login, logout, register } from "../controllers/handleAuth.js";
+import { login, logout, register } from "../controllers/entry.js";
 
 const router = Router();
 
